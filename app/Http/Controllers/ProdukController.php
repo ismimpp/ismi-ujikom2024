7dfb3560cadd579df29b591c361b18bc76dd35ad
ismimpp <issmimaileen@gmail.com>
2024-01-11 09:38:26 +0700

    apdet

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\produk;
+
 
 class ProdukController extends Controller
 {
-    //
+    //function produk(){
+    //    return view('Produk');
+    //}
+function Produk(){
+    $judul = "Data Produk";
+
+    $data = produk::all();
+
+    return view('Produk', [
+        'produk'-> $data,
+        'judul'-> $judul,
+    ]);
+}
+
+function update(){
+     
+}
+
+function hapus($id)
+{
+     echo $id;
+     $deleted = DB::table('produk')->where('ProdukID', $id)->delete();
+     if ($deleted) {
+        return redirect('/produk');
+     }
+}
 }
